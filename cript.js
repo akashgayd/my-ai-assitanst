e9fcc6a114cd6587084e4362e8eae1fe0f16b7ed
akash gaydhane <akashgaydhane781@gmail.com>
2024-10-24 22:39:17 +0530

    second commit

--- cript.js
+++ cript.js
@@ -61,23 +61,69 @@ recognition.onresult=(event)=>{
 
 btnEl.addEventListener("click",()=>{
 
+    btnEl.style.display = "none";
+
+    talkGif.style.display = "block";
     recognition.start();
+
 })
 
 
 function takeCommand(massage){
+
+    btnEl.style.display = "flex";
+     talkGif.style.display = "none";
+     askQuition.textContent = "ask me anything";
+
 
     if(massage.includes("hello")){
         speak("hello sir how can i help you?");
     }
     else if(massage.includes("who are you")){
 
-        speak("im shifra your persnonal vertual assitants created by akash sir");
+        speak("im shifra your personal virtual A i assistant created by akash sir");
     }
-    else if(massage.includes("open youtube")){
+    else if(massage.includes("youtube")){
 speak("opening youtube");
-
 window.open("https://www.youtube.com/","_blank")
         
     }
+    else if(massage.includes("facebook")){
+        speak("opening facebook");
+        window.open("https://www.facebook.com/","_blank")
+}
+
+else if(massage.includes("whatsapp")){
+    speak("opening whatsapp");
+    window.open("whatsapp://")
+}
+
+else if(massage.includes("instagram")){
+    speak("opening instagram");
+    window.open("instagram://")
+}
+
+else if(massage.includes("shapchat")){
+    speak("opening snapchat");
+    window.open("snapchat://")
+}
+
+else if(massage.includes("telegram")){
+    speak("opening telegram");
+    window.open("telegram://")
+}
+
+else if(massage.includes("youtube")){
+    speak("opening youtube");
+    window.open("youtube://")
+}
+else{
+    let finldSet =" this is i found the internet regarding"+ massage.replace("shipra","") || massage.replace("shifra","")||
+    massage.replace("shipera","")|| massage.replace("shifrra","");
+
+    speak(finldSet)
+    window.open(`https://www.google.com/search?q=${massage.replace("shipra","")}${massage.replace("sifra","")}`,"_blank")
+}
+
+
 }
